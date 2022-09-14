51cfeff0580a27912c398864e5aa9b1a4ad6629a
Michael Berrios <mxbics@rit.edu>
2022-09-14 13:31:43 -0400

    Completed Car Sale Commission Program

--- CarSalesCommission/Program.cs
+++ CarSalesCommission/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Name of the Seller: ");
+string nameOfSeller = Console.ReadLine();
+
+//  declaration of the amountOfSale variable and initialized with input value
+Console.Write("Enter amount of sale: ");
+double amountOfSale = double.Parse(Console.ReadLine());
+
+const double COMMISSION = .07; // 7% commission
+double salesPurchaseCommission = 0; // start 0 commission
+
+if(amountOfSale >= 10000) 
+{
+    salesPurchaseCommission = amountOfSale * COMMISSION;    
+}
+
+Console.WriteLine();
+Console.WriteLine("Salesperson: {0}", nameOfSeller);
+Console.WriteLine("Total Sale: {0:C}", amountOfSale);
+Console.WriteLine("Commission: {0:C}", salesPurchaseCommission);
