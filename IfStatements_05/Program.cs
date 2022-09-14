73733c731c26915914439b559502e5ced36099cc
Michael Berrios <mxbics@rit.edu>
2022-09-14 13:10:21 -0400

    New In Class Projects

---
+++ IfStatements_05/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Test that ((true && true) || false) is true by Ashley Locatelli");
+
+if((true && true) || false) 
+{
+    Console.WriteLine("Ashley was right");
+}
+else 
+{
+    Console.WriteLine("Ashley was wrong");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Test that (false && true || (true && false)) is false by Johnny Quinn");
+
+if(false && true || (true && false)) 
+{
+    Console.WriteLine("Johnny was wrong");
+}
+else 
+{
+    Console.WriteLine("Johnny was right");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Test that ((true && true || false) && true || (false)) is true by Erik Menchaca");
+
+if((true && true || false) && true || (false)) 
+{
+    Console.WriteLine("Erik was right");
+}
+else 
+{
+    Console.WriteLine("Erik was wrong");
+}
