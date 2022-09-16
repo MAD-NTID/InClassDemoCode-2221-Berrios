9e063a1be91179c5dde3514c801c856487d22cfd
Michael Berrios <mxbics@rit.edu>
2022-09-16 13:12:49 -0400

    TryParse and Switches

--- ATMMachine_06/Program.cs
+++ ATMMachine_06/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Please select the type of transaction you want from below\n" +
+"1) Deposit\n2) Balance Inquiry\n3) Withdraw\n");
+
+//  $1,500
+double balance = 1500;
+double deposit = 0;
+double withdraw = 0;
+
+string choice = Console.ReadLine();
+switch(choice)
+{
+    //  This is for depositing money
+    case "1":
+    case "Deposit":
+    case "deposit":
+    Console.Write("How much do you want to deposit? ");
+
+    deposit = double.Parse(Console.ReadLine());
+
+    balance += deposit;
+
+    Console.WriteLine($"You now have {balance:C}");
+    break;
+
+    //  This is for requesting how much money you have
+    case "2":
+    case "Balance Inquiry":
+    case "balance inquiry":
+
+    Console.WriteLine($"Your balance inquired is {balance:C}");
+    break;
+
+    //  This is for withdrawing money
+    case "3":
+    case "Withdraw":
+    case "withdraw":
+    Console.Write("How much do you want to withdraw? ");
+
+    withdraw = double.Parse(Console.ReadLine());
+
+    balance -= withdraw;
+
+    Console.WriteLine($"You now have {balance:C}");
+    break;
+}
--- TryParseAndSwitch_06/Program.cs
+++ TryParseAndSwitch_06/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("What is your age? ");
+
+//  this will force parse to a int variable
+//  However, if a non-numerical input is parsed for int, program will crash
+// int age = int.Parse(Console.ReadLine());
+
+//  What does tryParse actually returns?
+// int convertedNumber = 0;
+// Console.WriteLine(int.TryParse("five", out convertedNumber));
+
+// Console.WriteLine($"The parsed number is {convertedNumber}");
+
+int age = 0;
+bool validAge = int.TryParse(Console.ReadLine(), out age);
+
+if(validAge && age > 20) 
+{
+    Console.WriteLine("You are allowed to drink");
+}
+else if(validAge && age <= 20)
+{
+    Console.WriteLine("You are not allowed to drink");
+}
+else 
+{
+    Console.WriteLine("Sorry, that's an invalid input, it must be a whole positive number.");
+}
+
+Console.WriteLine();
+
+Console.Write("Enter your current bank balance: ");
+
+double bankBalance = 0;
+bool validBankBalance = double.TryParse(Console.ReadLine(), out bankBalance);
+
+if(validBankBalance && bankBalance > 0 && bankBalance < 1000000)
+{
+    Console.WriteLine("You have some money");
+}
+else if(validBankBalance && bankBalance >= 1000000)
+{
+    Console.WriteLine("You are filthy rich");
+}
+else
+{
+    Console.WriteLine("You are either broke, or you input a bad value");
+}
