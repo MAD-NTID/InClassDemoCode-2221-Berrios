ce577420d5f6a6e468b1c717a54326ae451a5c5f
michaelberrios-ntid <mxbics@rit.edu>
2022-10-17 13:24:14 -0400

    Added Substring Examples and Split method

--- StringTheory_12/Program.cs
+++ StringTheory_12/Program.cs
@@ -27,3 +27,34 @@ for(int i = 0; i < helloArrayOfChar.Length; i++)
 {
     Console.Write(helloArrayOfChar[i]);
 }
+
+Console.WriteLine();
+
+// How to create a swap names (the long way, without using the Split method)
+string originalName = "John Doe";
+string firstname = "";
+string lastname = "";
+
+// for(int i = 0; i < originalName.Length; i++)
+// {
+//     if(originalName[i] == ' ')
+//     {
+//         lastname = originalName.Substring(i + 1) + ", ";
+//         break;
+//     }
+//     else
+//     {
+//         firstname += originalName[i];
+//     }
+// }
+
+// string newName = lastname + firstname;
+
+// Console.WriteLine(newName);
+
+//  The short way using the Split method
+string[] names = "John Doe".Split(" ");
+
+string newName = names[1] + ", " + names[0];
+
+Console.WriteLine(newName);
