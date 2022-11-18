7d0865d16ef7b371cb9be7e32dbf68f1d26e0177
michaelberrios-ntid <mxbics@rit.edu>
2022-11-18 12:34:53 -0500

    Completed StreamReader

---
+++ StreamReader/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Enter a file name: ");
+string filename = Console.ReadLine();
+
+if(!string.IsNullOrEmpty(filename))
+    filename += ".txt"; //  .txt is a globally accepted file format by most computer platforms
+
+if(File.Exists(filename))
+{
+    StreamReader reader = new(filename);
+
+    //  Print the first line, and move to the next line
+    // Console.WriteLine(
+    //     reader.ReadLine()
+    // );
+
+    // Console.WriteLine(
+    //     reader.ReadLine()
+    // );
+
+    // //  EndOfStream returns a bool of whether or not we are in the last line of the file
+    // Console.WriteLine(
+    //     reader.EndOfStream
+    // );
+    while(!reader.EndOfStream)
+    {
+        Console.WriteLine(
+            reader.ReadLine()
+        );
+    }
+
+    reader.Close();
+
+    Console.WriteLine($"Done reading {filename}");
+}
+else
+{
+    Console.WriteLine($"File {filename} was not found");
+}
